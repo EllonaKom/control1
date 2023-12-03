48be44929501fcd00ae40acb5f4a0932cd77c5cc
Ellona <el_br10@mail.ru>
2023-12-03 23:18:26 +0300

    Программа и файл readme.md добавлены

--- Proga1/Program.cs
+++ Proga1/Program.cs
@@ -2,8 +2,37 @@
  длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
  либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
  лучше обойтись исключительно массивами.
-
 Примеры:
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-[“Russia”, “Denmark”, “Kazan”] → []*/ 
+[“Russia”, “Denmark”, “Kazan”] → []*/
+
+void ArraySecond(string[] array1, string[] array2)
+{
+    int y = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[y] = array1[i];
+            y++;
+        }
+    }
+}
+void PrintArray(string message, string[] array)
+{
+    Console.WriteLine(message);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array1 = new string[7] { "Hello", "2", "1234", "world", ":-)", "03", "Russia" };
+string[] array2 = new string[array1.Length];
+
+ArraySecond(array1, array2);
+
+PrintArray("Исходный массив : ", array1);
+PrintArray("Получившийся массив :", array2);
